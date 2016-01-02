ef602603579a8b3c4f12bdcfc93d5875884152b6
Savlon <davie_sav27@hotmail.com>
2016-01-02 13:33:26 +1000

    instrument

--- DeadmanTwo/Assets/Scripts/Item.cs
+++ DeadmanTwo/Assets/Scripts/Item.cs
@@ -3,6 +3,23 @@ using System.Collections;
 
 public class Item
 {
-	public string hello;
+	private string _name;
+	private string _description;
 
+
+
+	public virtual bool InteractOn (Tile tile, Level level, int x, int y, Player player)
+	{
+		return false;
+	}
+
+	public virtual bool Interact (Player player, Entity entity)
+	{
+		return false;
+	}
+
+	public virtual bool CanAttack ()
+	{
+		return false;
+	}
 }
