746e52965061aeddd951036245576adbda9430ff
Savlon <davie_sav27@hotmail.com>
2016-01-20 20:24:54 +1000

    Rock added

---
+++ DeadmanTwo/Assets/Scripts/Rock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : Tile
+{
+	public Rock (int id, bool passable = true, bool isBaseTile = false) : base (id, passable, isBaseTile)
+	{
+		
+	}
+
+	public override bool Interact (Level level, int x, int y, Player player, Item item)
+	{
+		if (item is Instrument)
+		{
+			Instrument instrument = (Instrument)item;
+			if (instrument.Type == InstrumentType.PICKAXE)
+			{
+				level.SetTile (x, y, TileDatabase.AIR, 0);
+				return true;
+			}
+		}
+		return base.Interact (level, x, y, player, item);
+	}
+}
+
